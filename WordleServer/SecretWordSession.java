62ee6c056ce2ab0c24792258bca9e3b150a99020
Lupetti Lorenzo <lorenzo.lupetti3@gmail.com>
2022-12-12 17:12:04 +0100

    Aggiunto il progetto

---
+++ WordleServer/SecretWordSession.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SecretWordSession { // rappresenta la sessione specifica a una parola, quindi ha la parola segreta, la durata della sessione in data e la lista di utenti che hanno/stanno giocando
+    public volatile StringBuilder wordDurationDate; // range in data della durata della parola
+    public volatile StringBuilder secretWord; // parola segreta
+    // mappa delle sessioni degli utenti
+    // un utente crea una sessione per la parola corrente quando manda gioca per la prima volta
+    public volatile Map<String, UserSession> usersSessionMap;
+
+    public SecretWordSession() {
+        secretWord = new StringBuilder();
+        wordDurationDate = new StringBuilder();
+        usersSessionMap = new HashMap<>();
+    }
+}
